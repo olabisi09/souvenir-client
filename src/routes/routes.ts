a7b288e28aa1fb9091955fe04e67e4727b62ef73
olabisiolaoye10 <olabisiolaoye10@yahoo.com>
2022-10-28 10:11:27 +0100

    folders

---
+++ src/routes/routes.ts
@@ -0,0 +1,11 @@
+export const routes = {
+    index: '/',
+    login: '/auth/login',
+    register: '/auth/register',
+    feedback: '/feedback',
+    shop: '/shop',
+    profile: '/profile/{username}',
+    cart: '/cart',
+    checkout: '/checkout',
+    //payment: '/payment'
+}
